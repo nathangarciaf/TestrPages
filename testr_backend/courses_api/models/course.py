418a6968ebac42c45ed3044804c17cc8b5635e7e
nath <nathangarfreitas@gmail.com>
2024-05-30 19:12:28 -0300

    backend update

--- testr_backend/courses_api/models/course.py
+++ testr_backend/courses_api/models/course.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.utils import timezone
-from django.utils.translation import gettext_lazy as _
+from django.contrib.auth.models import User
 import uuid
 
 class Course(models.Model):
